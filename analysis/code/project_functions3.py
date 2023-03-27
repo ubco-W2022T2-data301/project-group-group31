6f8f8ae5d81e9eab9e57f34c480207c05d0222c5
Chase Winslow <chasew13579@gmail.com>
2023-03-27 13:08:03 -0700

    Work done on A3

--- analysis/code/project_functions3.py
+++ analysis/code/project_functions3.py
@@ -1,21 +1,50 @@
 import pandas as pd
 
+# Mapping positions to forward (F) and defense (D)
+positions_map = {
+    'RW': 'F',
+    'C': 'F',
+    'LW': 'F',
+    'C/LW': 'F',
+    'LW/C': 'F',
+    'C/RW': 'F',
+    'W': 'F',
+    'RW/D': 'F',
+    'C RW': 'F',
+    'C; LW': 'F',
+    'LD': 'D',
+    'RD': 'D',
+    'D': 'D',
+    'LW/D': 'D',
+    'RW/D': 'D'
+}
+
+# Load dataframe and replace positions
 def load_and_process(path_to_csv_file):
     #Load dataframe from given path
     df = pd.read_csv(path_to_csv_file)
     df = (df.loc[:9927]
-      .drop(['id', 'age', 'plus_minus', 'penalties_minutes', 'goalie_losses', 'to_year',
-             'goalie_ties_overtime', 'save_percentage', 'goals_against_average',
+       .drop(['id', 'age', 'plus_minus', 'penalties_minutes', 'goalie_losses','to_year',
+             'goalie_ties_overtime', 'save_percentage', 'goals_against_average', 'player',
              'point_shares', 'goals', 'assists','team','overall_pick','nationality','goalie_games_played'], axis=1)
-      .assign(score=lambda x: x.games_played + x.points+x.goalie_wins)
-      .dropna(subset=['games_played'], how='all')
+      .rename(columns={'year': 'Year','position': 'Position', 'amateur_team': 'Amateur Team', 
+                       'games_played': 'Games', 'points': 'Points', 'goalie_wins': 'Goalie Wins'})
+      .assign(Score=lambda x: x.Games + x.Points)
+      .dropna(subset=['Games'], how='all')
      )
-    df['amateur_team'] = df['amateur_team'].replace('Portland Winter Hawks (WHL)', 'Portland Winterhawks (WHL)')
-    df['amateur_team'] = df['amateur_team'].replace('USA U-18 Development Team (USDP/USHL)', 'USNDT (USA)')
-    df['amateur_team'] = df['amateur_team'].replace('USA U-18 Development Team (USDP/NAHL)', 'USNDT (USA)')
-    df['amateur_team'] = df['amateur_team'].replace('CSKA Moskva (Soviet)', 'CSKA Moskva (Russia)')
-    df['amateur_team'] = df['amateur_team'].replace('Dynamo Moskva (Soviet)', 'Dynamo Moskva (Russia)')
+    #Fixing teams that changed their name 
+    df['Amateur Team'] = df['Amateur Team'].replace('Portland Winter Hawks (WHL)', 'Portland Winterhawks (WHL)')
+    df['Amateur Team'] = df['Amateur Team'].replace('USA U-18 Development Team (USDP/USHL)', 'USNDT (USA)')
+    df['Amateur Team'] = df['Amateur Team'].replace('USA U-18 Development Team (USDP/NAHL)', 'USNDT (USA)')
+    df['Amateur Team'] = df['Amateur Team'].replace('CSKA Moskva (Soviet)', 'CSKA Moskva (Russia)')
+    df['Amateur Team'] = df['Amateur Team'].replace('Dynamo Moskva (Soviet)', 'Dynamo Moskva (Russia)')
+    
+    # Replace positions
+    df['Position'] = df['Position'].replace(positions_map)
+    
     return df
+
+
 
 
 
